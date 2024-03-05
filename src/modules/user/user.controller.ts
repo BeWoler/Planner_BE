d3680ff19a27dd97a78cd993a654c29b91573511
BeWoler <bewoler.del@gmail.com>
2024-03-05 17:51:12 +0300

    added user service and controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common'
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
 import { UserService } from './user.service'
+import { CurrentUser } from 'src/decorators/user.decorator'
+import { Auth } from 'src/decorators/auth.decorator'
+import { UserDto } from './dto/user.dto'
 
-@Controller('user')
+@Controller('user/profile')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
+
+	@Get()
+	@Auth()
+	async profile(@CurrentUser('id') id: string) {
+		return this.userService.getProfile(id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Put()
+	@Auth()
+	async updateProfile(@CurrentUser('id') id: string, @Body() dto: UserDto) {
+		return this.userService.update(id, dto)
+	}
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common'
 import { hash } from 'argon2'
 import { AuthDto } from 'src/auth/dto/auth.dto'
 import { PrismaService } from 'src/prisma.service'
+import { UserDto } from './dto/user.dto'
+import { startOfDay, subDays } from 'date-fns'
 
 @Injectable()
 export class UserService {
@@ -16,6 +18,52 @@ export class UserService {
 				tasks: true
 			}
 		})
+	}
+
+	async getProfile(id: string) {
+		const profile = await this.getById(id)
+
+		const totalTasks = profile.tasks.length
+		const completedTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				isCompleted: true
+			}
+		})
+
+		const todayStart = startOfDay(new Date())
+		const weekStart = startOfDay(subDays(new Date(), 7))
+
+		const todayTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: {
+					gte: todayStart.toISOString()
+				}
+			}
+		})
+
+		const weekTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: {
+					gte: weekStart.toISOString()
+				}
+			}
+		})
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { password, ...rest } = profile
+
+		return {
+			user: rest,
+			statistics: [
+				{ label: 'Total', value: totalTasks },
+				{ label: 'Completed tasks', value: completedTasks },
+				{ label: 'Today tasks', value: todayTasks },
+				{ label: 'Week tasks', value: weekTasks }
+			]
+		}
 	}
 
 	async getByEmail(email: string) {
@@ -34,5 +82,24 @@ export class UserService {
 		}
 
 		return this.prisma.user.create({ data: user })
+	}
+
+	async update(id: string, dto: UserDto) {
+		let data = dto
+
+		if (dto.password) {
+			data = { ...dto, password: await hash(dto.password) }
+		}
+
+		return this.prisma.user.update({
+			where: {
+				id
+			},
+			data,
+			select: {
+				name: true,
+				email: true
+			}
+		})
 	}
 }
